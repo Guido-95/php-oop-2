de5eab9a672426d056e37366c8ec31397ac65f45
Guido-95 <guidocarna1995@gmail.com>
2021-07-09 17:33:34 +0200

    esercitazione

--- index.php
+++ index.php
@@ -10,11 +10,10 @@ $utnPrm2 = new Premium('Marco','Salotto','Via Roma 2',3);
 
 $prodotto = new TipoProdotto(124213 , 1099.99,"PC desktop msi",'Informatica');
 $prodotto2 = new TipoProdotto(12342 , 24.99,"Tavolo", 'Cucina');
-// $utnPrm1
+
+
 var_dump($prodotto);
 var_dump($prodotto2);
-
-
 var_dump($utn);
 var_dump($utnPrm1);
 var_dump($utnPrm2);
